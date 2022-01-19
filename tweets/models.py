50d9a424b939bb4db28b7ea78249977b2f89357d
JorisPaarde <jorispaarde@gmail.com>
2022-01-19 16:57:14 +0100

    created tweet app

---
+++ tweets/models.py
@@ -0,0 +1,8 @@
+from email.mime import image
+from django.db import models
+
+
+
+class Tweet(models.Model):
+    content = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
